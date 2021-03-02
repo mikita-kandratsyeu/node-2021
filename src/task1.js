823fb554aaaa64925f5e898a9b099c9bc4f9a9fd
Mikita Kandratsyeu <Mikita_Kandratsyeu1@epam.com>
2021-03-02 22:02:50 +0300

    add task-1

--- src/task1.js
+++ src/task1.js
@@ -1,1 +1,17 @@
-console.log('Task1')
+import process from 'process';
+
+process.stdin.setEncoding('utf-8');
+
+process.stdin.on('readable', () => {
+  const sentence = process.stdin.read();
+
+  if (sentence) {
+    process.stdout.write(`${[...sentence]
+      .filter(item => item !== '\n' && item !== '\r')
+      .reverse()
+      .join('')}\r\n`
+    );
+  }
+
+  process.exit(0);
+});
