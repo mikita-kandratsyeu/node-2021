2860d654d0fc6e9693f059975ff74854362fe1b6
Mikita Kandratsyeu <Mikita_Kandratsyeu1@epam.com>
2021-03-05 18:03:58 +0300

    add task-2

--- src/task2.js
+++ src/task2.js
@@ -1,1 +1,22 @@
-console.log('Task2')
+import csv from 'csvtojson';
+import fs from 'fs';
+import path from 'path';
+import { pipeline } from 'stream';
+
+const filePath = path.resolve('csv', 'input.csv');
+const resultPath = path.resolve('output', 'output.txt');
+
+pipeline(
+  fs.createReadStream(filePath, { encoding: 'utf-8' }),
+  csv(),
+  fs.createWriteStream(resultPath),
+  err => {
+    if (err) {
+      console.error(`Pipeline failed: `, err);
+    } else {
+      console.info('Pipeline succeeded!')
+    }
+  }
+);
+
+
