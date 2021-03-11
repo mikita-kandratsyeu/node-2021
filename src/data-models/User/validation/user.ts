944de2920ea9ef27d0046e0b73c3a579c00ea686
Mikita Kandratsyeu <Mikita_Kandratsyeu1@epam.com>
2021-03-11 13:33:56 +0300

    add some changes

--- src/data-models/User/validation/user.ts
+++ src/data-models/User/validation/user.ts
@@ -3,6 +3,6 @@ import joi from 'joi';
 export const user = joi.object({
   login: joi.string().required(),
   password: joi.string().pattern(new RegExp('^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$')).required(),
-  age: joi.number().integer().min(4).max(120).required(),
+  age: joi.number().integer().min(4).max(130).required(),
   isDeleted: joi.boolean().required(),
 });
